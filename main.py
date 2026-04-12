148057f497c901df7d6085b16914c70a01000570
fetchingrhombus <fetchingrhombus@gmail.com>
2026-04-12 17:34:10 -0500

    C: Made a main window pop up, and made it closeable by user

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 import pygame
 from constants import *
 # or import everything: from module_name import *
+from logger import log_state
+
 
 
 def main():
     print("Starting Asteroids with pygame version:", pygame.version.ver)
     print("Screen width:",SCREEN_WIDTH)
     print("Screen height:",SCREEN_HEIGHT)
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while(True):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        log_state()
+        #pygame.Surface.fill("black")
+        pygame.Surface.fill(screen, (0,0,0))
+        pygame.display.flip()
+
+
+
+
+
 
 if __name__ == "__main__":
     main()
